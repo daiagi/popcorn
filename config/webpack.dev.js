d7d7a7eac8fa1de99b566388d9c8c8d66f1aeab5
Baruch <daiagi@gmail.com>
2020-04-26 16:19:00 +0700

    WebPack config
    add rules to load global css and css modules
    add dotenv

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,23 +1,42 @@
 const merge = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
+
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig,
-  {
-    mode: 'development',
-    devServer: {
-      hot: true,
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-          ],
+    {
+        mode: 'development',
+        devServer: {
+            hot: true,
         },
-      ],
-    },
-  });
+        devtool: 'inline-source-map',
+        plugins: [
+            new Dotenv()
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: true,
+                            },
+                        },
+                    ],
+                    include: /\.module\.css$/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                    ],
+                    exclude: /\.module\.css$/,
+                },
+            ],
+        },
+    });
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,53 +9,59 @@ const commonConfig = require('./webpack.common.js');
 
 
 module.exports = merge(commonConfig,
-  {
-    mode: 'production',
-    output: {
-      filename: '[name].bundle.[contenthash].js',
-    },
-    devtool: 'source-map',
-    plugins: [
-      new CleanWebpackPlugin(),
-      new MiniCssExtractPlugin(
-        {
-          filename: '[name].[contenthash].css',
+    {
+        mode: 'production',
+        output: {
+            filename: '[name].bundle.[contenthash].js',
         },
-      ),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            'css-loader',
-          ],
+        devtool: 'source-map',
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin(
+                {
+                    filename: '[name].[contenthash].css',
+                },
+            ),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: true,
+                            },
+                        },
+                    ],
+                },
+            ],
         },
-      ],
-    },
 
 
-    optimization: {
-      minimizer: [
-        new OptimizeCssAssetsPlugin({
-          cssProcessorOptions: {
-            map: {
-              inline: false,
-              annotation: true,
-            },
-          },
-        }),
-        new TerserPlugin({
-          // Use multi-process parallel running to improve the build speed
-          // Default number of concurrent runs: os.cpus().length - 1
-          parallel: true,
-          // Enable file caching
-          cache: true,
-          sourceMap: true,
-        }),
-      ],
-    },
+        optimization: {
+            minimizer: [
+                new OptimizeCssAssetsPlugin({
+                    cssProcessorOptions: {
+                        map: {
+                            inline: false,
+                            annotation: true,
+                        },
+                    },
+                }),
+                new TerserPlugin({
+                    // Use multi-process parallel running to improve the build speed
+                    // Default number of concurrent runs: os.cpus().length - 1
+                    parallel: true,
+                    // Enable file caching
+                    cache: true,
+                    sourceMap: true,
+                }),
+            ],
+        },
 
 
-  });
+    });
