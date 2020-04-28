6cb69e82ad35ca113064620edd493c4733efb901
Baruch <daiagi@gmail.com>
2020-04-28 14:34:30 +0700

    design poster card

--- src/componenets/card/card.jsx
+++ src/componenets/card/card.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 import style from './card.module.css';
 
 const getPosterImg = (title, posterPath) => {
@@ -17,17 +18,35 @@ const getPosterImg = (title, posterPath) => {
         <img
             src={`${secureBaseUrl}${posterSizes.w500}${posterPath}`}
             alt={title}
+            className={style.posterImg}
         />
     );
 };
 
 const Card = (props) => {
     const { entry } = props;
+    const starRating = entry.vote_average / 2;
     return (
         <div key={entry.id} className={style.posterCard}>
             {getPosterImg(entry.title, entry.poster_path)}
             <div className={style.overlay}>
-                Text here
+                <p className={style.title}>
+                    {entry.title}
+                </p>
+                <p className={`${style.title} ${style.year} `}>
+                    {entry.release_date.substring(0, 4)}
+                </p>
+                <div className={style.starRating}>
+                    <StarRatings
+                        rating={starRating}
+                        starRatedColor="#0b0c0c"
+                        starEmptyColor="#bdc4cb"
+                        starDimension="20px"
+                        starSpacing="1px"
+                        numberOfStars={5}
+                        name="rating"
+                    />
+                </div>
             </div>
 
         </div>
