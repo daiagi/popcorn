08422580c0539ef7765116e4f00c372d3856850c
Baruch <daiagi@gmail.com>
2020-05-02 14:05:56 +0700

    ESLint rule: allow devDeps import

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,14 +23,17 @@ module.exports = {
   ],
   rules: {
     "no-console": "warn",
+    "import/no-extraneous-dependencies": [
+      "error", { "devDependencies": true,
+        }],
     "react/jsx-indent": [1, 2],
     "comma-dangle": 0,
     "indent": [1, 2],
     "react/jsx-indent-props": [1, 2],
     "react/prop-types": 0,
-    "no-unused-expressions": ["error", { 
-      "allowShortCircuit": true, 
-      "allowTernary": true, 
-      }]
+    "no-unused-expressions": ["error", {
+      "allowShortCircuit": true,
+      "allowTernary": true,
+    }]
   },
 };
