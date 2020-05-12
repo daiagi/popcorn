410ff001535906d7a0b192e990bf4317435c06ac
Baruch <daiagi@gmail.com>
2020-05-12 13:18:14 +0700

    remove utils file

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-const fetchWithTimeout = (timeout = 7000) => (url, options) => Promise.race([
-  fetch(url, options),
-  new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), timeout))
-]).catch(console.error);
-
-
-export default fetchWithTimeout;
